e2a54873629d3c756b1d22eceedaa1952e77df14
setTileGroup <2077361504@qq.com>
2015-02-14 23:09:45 +0800

    upadte

--- javascripts/main.js
+++ javascripts/main.js
@@ -22,26 +22,35 @@ var checkedid=false;
 checkidentity();
 function checkidentity()
 {
+	//检查访问者是否有正确的cookies
+	/*  //add cookies
+		var exdate=new Date();
+		exdate.setDate(exdate.getDate()+1000);
+		document.cookie="identity"+ "=" +escape("gia'omcseentgtyiilheugircohuepnmgeymubaenr")+
+			((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+	*/
 	var idcheck=getCookie("identity");
 	var substr=["gaocengyihuichengyuan","i'msettilegroupmember"];
 	var trueidstr="";
+	var a1=document.getElementsByTagName("body");
 	for(var i=0;i<substr[0].length;i++)
 	{
 		trueidstr+=substr[0][i]+substr[1][i];
 	}
-	if(idcheck!=trueidstr)
+	if(a1.length!=0)
 	{
-		var a1=document.getElementsByTagName("body");
-		if(a1.length!=0)
-		{
+		if(idcheck!=trueidstr)
+		{	
 			a1[0].innerHTML="<p>Welcome to setTileGroup high-rise website. You do not have permission to view this site. Ask setTileGroup administrators for more imformation. </p>"+
-			"<p>欢迎您访问setTile工作室高层内部网站。你没有权限访问本站。请向setTile工作室的管理员咨询。</p>";
+			"<p>欢迎您访问setTile工作室高层内部网站。你没有权限访问本站内容。请向setTile工作室的管理员咨询。</p>";
+			console.log("You were blocked because you do not have the correct identity. ");
 			checkedid=true;
 		}
-	}
-	else
-	{
-		checkedid=true;
+		else
+		{
+			checkedid=true;
+		}
+		1[0].style.display="inline";
 	}
 	if(!checkedid)
 	{
